e956e3294081be6cc4efa92a27b49574c688a0ea
Palak Sharma <palak412005sharma@gmail.com>
2026-05-16 16:28:15 +0530

    commit

--- frontend/src/components/navbar/Navlinks.jsx
+++ frontend/src/components/navbar/Navlinks.jsx
@@ -1,44 +1,4 @@
-// import React from 'react'
-// import { NavLink } from "react-router-dom";
 
-// const Navlinks = ({ mobile = false, closeMenu }) => {
-//     const links = [ { name: "Home", path: "/" },
-//          { name: "Projects", path: "/projects" },
-//           { name: "Blog", path: "/blog" },
-//            { name: "Login", path: "/login" }, 
-//            { name: "Signup", path: "/signup" },
-//         { name: "Contact", path: "/contact" },
-//     { name: "About", path: "/about" }, ];
-//   return (
-//      <ul
-//       className={`flex ${
-//         mobile
-//           ? "flex-col gap-6 text-center"
-//           : "flex-row items-center gap-8"
-//       }`}
-//     >
-//       {links.map((link) => (
-//         <li key={link.name}>
-//           <NavLink
-//             to={link.path}
-//             onClick={closeMenu}
-//             className={({ isActive }) =>
-//               `transition duration-300 font-medium hover:text-cyan-400 ${
-//                 isActive
-//                   ? "text-cyan-400"
-//                   : "text-white"
-//               }`
-//             }
-//           >
-//             {link.name}
-//           </NavLink>
-//         </li>
-//          ))}
-//     </ul>
-//   )
-// }
-
-// export default Navlinks
 
 import { Link } from "react-router-dom";
 
